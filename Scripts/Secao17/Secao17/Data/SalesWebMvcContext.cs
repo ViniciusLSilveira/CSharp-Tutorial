9d4a1218da2ad89f77aba0cf9ed2b6ef3e40f674
Vinícius de Lima Silveira <ViniciusLSilveiraDev@gmail.com>
2020-02-24 17:13:09 -0300

    Adicionado outras entidades e Segunda migração

--- Scripts/Secao17/Secao17/Data/SalesWebMvcContext.cs
+++ Scripts/Secao17/Secao17/Data/SalesWebMvcContext.cs
@@ -14,6 +14,8 @@ namespace Secao17.Data
         {
         }
 
-        public DbSet<Secao17.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
