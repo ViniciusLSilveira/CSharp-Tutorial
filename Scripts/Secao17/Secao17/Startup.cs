dd1f50ebd9f2e542ba71d29d94548b986cda9da3
Vinícius de Lima Silveira <ViniciusLSilveiraDev@gmail.com>
2020-02-24 17:40:48 -0300

    Seeding Service
    Adicionado um serviço para popular o banco de dados caso não haja nenhum dado

--- Scripts/Secao17/Secao17/Startup.cs
+++ Scripts/Secao17/Secao17/Startup.cs
@@ -39,14 +39,17 @@ namespace Secao17
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                     builder.MigrationsAssembly("Secao17")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
