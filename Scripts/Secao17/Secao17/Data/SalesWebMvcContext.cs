fbaf1ead77210e7cbfe970f15ebe85e5b53f0546
Vinícius de Lima Silveira <ViniciusLSilveiraDev@gmail.com>
2020-02-24 15:36:13 -0300

    CRUD Scaffolding

---
+++ Scripts/Secao17/Secao17/Data/SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Secao17.Models;
+
+namespace Secao17.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Secao17.Models.Department> Department { get; set; }
+    }
+}
--- Scripts/Secao17/Secao17/Startup.cs
+++ Scripts/Secao17/Secao17/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Secao17.Data;
 
 namespace Secao17
 {
@@ -33,6 +35,9 @@ namespace Secao17
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
