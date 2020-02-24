b7bd07650428490858192a5371b933e1a508c957
Vinícius de Lima Silveira <ViniciusLSilveiraDev@gmail.com>
2020-02-24 18:21:54 -0300

    Criação de formulário simples

--- Scripts/Secao17/Secao17/Controllers/SellersController.cs
+++ Scripts/Secao17/Secao17/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Secao17.Services;
+using Secao17.Models;
 
 namespace Secao17.Controllers
 {
@@ -22,6 +23,20 @@ namespace Secao17.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
