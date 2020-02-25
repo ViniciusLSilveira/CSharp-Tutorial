3718d652adc4ef0efbd6bc4be0efd8deb6ef2444
Vinícius de Lima Silveira <ViniciusLSilveiraDev@gmail.com>
2020-02-24 23:12:04 -0300

    Implementado SimpleSearch, GroupingSearch

--- Scripts/Secao17/Secao17/Models/SalesRecord.cs
+++ Scripts/Secao17/Secao17/Models/SalesRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Secao17.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Secao17.Models
 {
@@ -7,7 +8,11 @@ namespace Secao17.Models
     {
 
         public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
